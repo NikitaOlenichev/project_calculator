818f575b4ffdc52f4f322b29a762ad6625951786
Никита Оленичев <2020nikol@mail.ru>
2026-05-16 10:00:22 +0300

    feat: implement interactive console UI

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def main():
     try:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
+        print("Выберите функцию:\n1. Сложение;\n2. Вычитание.")
+        value = int(input("Введите номер функции: "))
+        match value:
+            case 1:
+                print(f"Результат: {a} + {b} = {add(a, b)}")
+            case 2:
+                print(f"Результат: {a} - {b} = {subtract(a, b)}")
+            case _:
+                print("Неизвестная функция!!!!")
     except ValueError:
         print("Ошибка: пожалуйста, введите числа.")
 
